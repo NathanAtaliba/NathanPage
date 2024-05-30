0aaf7d3b91b1f06631f614e8329e0429f7b1a45a
NathanAtaliba <nathanataliba28@gmail.com>
2024-05-30 18:45:56 -0300

    configurando pages

---
+++ docs/JS/contact.js
@@ -0,0 +1,19 @@
+function home(){
+    window.location.href = "../index.html"
+}
+
+function contact(){
+    window.location.href = "../pages/contact.html"
+}
+
+function projects(){
+    window.location.href = "../pages/projects.html"
+}
+
+function technology(){
+    window.location.href = "../pages/technology.html"
+}
+
+function about(){
+    window.location.href = "../pages/about.html"
+}
