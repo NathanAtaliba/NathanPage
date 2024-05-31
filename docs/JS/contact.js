844cc4c5a83c362b723f9719e1aaf2809e375f41
NathanAtaliba <nathanataliba28@gmail.com>
2024-05-31 00:40:08 -0300

    pages add text and icons

--- docs/JS/contact.js
+++ docs/JS/contact.js
@@ -17,3 +17,19 @@ function technology(){
 function about(){
     window.location.href = "../pages/about.html"
 }
+
+function instagram(){
+    window.location.href = "https://www.instagram.com/nathan_ataliba11/"
+}
+
+function facebook(){
+    window.location.href = "https://www.facebook.com/nathan.ataliba?locale=pt_BR"
+}
+
+function linkedin(){
+    window.location.href = "https://www.linkedin.com/in/nathan-ataliba-92899223a/"
+}
+
+function github(){
+    window.location.href = "https://github.com/NathanAtaliba"
+}
